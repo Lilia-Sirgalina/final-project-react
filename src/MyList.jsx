59631e49d0a583a8e3c1e949d8588999aae4cd4d
Lilia-Sirgalina <lilia_developer@hotmail.com>
2026-02-17 16:06:02 +0100

    Changes 17/02

--- src/MyList.jsx
+++ src/MyList.jsx
@@ -1,18 +1,19 @@
-const MyList = ({addMeal, mealPlans, deleteDay, selectedDay, setSelectedDay}) => {
-
-    
+const MyList = ({addMeal, mealPlans, deleteDay, selectedDay, setSelectedDay}) => {    
 
     return (
         <div>
             <div>
                 <h1>Weekly Meal Plan Ideas</h1>                
-                <button className="button-add" onClick={addMeal}>Add</button>
-                
+                <button className="button-add" onClick={addMeal}>Add</button>                
             </div>
 
             <div>
                 {mealPlans.map(({id, title, mealForADay}) => (
-                    <div key={id} className={selectedDay ? {backgroundColor: "white"} : {backgroundColor: "yellow"}}>
+                    <div key={id} 
+                        onClick={() => setSelectedDay(id)} 
+                        className={`${selectedDay === id ? "selected" : "default"}`}
+                        >
+                            
                         <p>{title}</p>
                         <p>{mealForADay}</p>
                         <button className="button-delete" onClick={() => deleteDay(id)}>Delete</button>
